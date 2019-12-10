4b7c62d92e4031371c4af848c9879d50736a28d8
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-12-10 01:45:19 +0100

    calling the weather function

--- src/App.js
+++ src/App.js
@@ -5,19 +5,44 @@ import Weather from "./components/Weather/Weather";
 
 const api_key = "e876ad8eedce4610f913d17aaaadae72"
 
+
+
 // api.openweathermap.org/data/2.5/weather?q={city name},{country code}
 const App = () => {
 
-    // const [weather, setWeather] = useState();
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+    // const [icon, setIcon] = useState();
+    const [description, setDescription] = useState("");
+    const [temp_min, setTemp_min] = useState("");
+    const [temp_max, setTemp_max] = useState("");
+    const [temp, setTemp] = useState("");
+
 
     const getWeather = async () => {
-        const api_call = await axios.get(`// api.openweathermap.org/data/2.5/weather?q={London},{uk}&appid=${api_key}`);
-        // const response = await api_call.
+        const api_call = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${api_key}`);
+        const response = await api_call;
+        console.log(response);
+        // return response;
+        setCity(response.data.name);
+        setCountry(response.data.sys.country);
+        setTemp(response.data.main.temp);
+        setTemp_max(response.data.main.temp_max);
+        setTemp_min(response.data.main.temp_min);
+        setDescription(response.data.weather[0].description);
     }
+    getWeather();
     
     return (
         <div className="App">
-            <Weather />
+            <Weather 
+                city = {city} 
+                country = {country} 
+                temp = {temp} 
+                temp_max = {temp_max}
+                temp_min = {temp_min}
+                description = {description}
+            />
         </div>
     )
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,19 +1,37 @@
 import React from "react";
 import "./Weather.css";
 
-const Weather = () => (
-    <>
-        <div className="box">
-            <div>
-                <input type="text" placeholder="Enter country" />
-                <input type="text" placeholder="Enter city" />
-                <span class="button"><button type="submit" value="search">Get Weather</button></span>
-            </div>
-        </div>
-        <div className="display-weather">
-            <h1>25&deg;</h1>
-        </div>
-    </>
-)
+
+
+const Weather = 
+    ({ 
+        city, 
+        country,
+        temp,
+        temp_max,
+        temp_min,
+        description,
+        loadWeather
+    }) => {
+        return (
+            <>
+                <div className="box">
+                    <div>
+                        <input type="text" placeholder="Enter country" />
+                        <input type="text" placeholder="Enter city" />
+                        <span className="button" onClick={loadWeather}><button type="submit" value="search">Get Weather</button></span>
+                    </div>
+                </div>
+                <div className="display-weather">
+                    <div>
+                        <span>{ Math.floor(temp) }&deg;</span>
+                        <span><h2>{ city }, { country }</h2></span>
+                        <span><h3>{ Math.floor(temp_min) }&deg; - { Math.floor(temp_max) }&deg;</h3></span>
+                        <span><h3>{ description }</h3></span>
+                    </div>
+                </div>
+            </>
+        );
+}
 
 export default Weather;
